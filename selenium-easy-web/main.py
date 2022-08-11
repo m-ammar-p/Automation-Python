8d9074b89061062979279096a50de7cb9c910ffd
m-ammar-p <ma.paracha@hotmail.com>
2022-08-12 02:33:59 +0500

    Added webdriver for mac os

--- selenium-easy-web/main.py
+++ selenium-easy-web/main.py
@@ -2,19 +2,24 @@ import os
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import  WebDriverWait
-from selenium.webdriver.support import  expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def selenium_easy():
 
     # for windows add driver path like this
-    os.environ['PATH'] += r"F:/SeleniumWebdrivers"
+    # os.environ['PATH'] += r"F:/SeleniumWebdrivers"
+    # driver = webdriver.Chrome()
+
+    # for mac os
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    DRIVER_BIN = os.path.join(PROJECT_ROOT, "/usr/local/bin/chromedriver")
     driver = webdriver.Chrome()
 
     driver.get("https://jqueryui.com/resources/demos/progressbar/download.html")
     # waiting for 3 seconds if the element is already there it will not wait for 3 seconds
-    # it will git initbe applied across all the elements
+    # it will be applied across all the elements
     driver.implicitly_wait(3)
 
     download_btn = driver.find_element(By.ID, value='downloadButton')
@@ -26,7 +31,8 @@ def selenium_easy():
                 # Element Filtration
                 (By.CLASS_NAME, 'progress-label'),
                 'Complete!'  # The Expected Text
-            )
+            ),
+            print("Text Found = Complete!")
         )
     except Exception as e:
         print(str(e) + "Could not find text")
